416480a28bf4d1909e08e967201ff8e0c7c0d6d6
Markus <07mhjoa@gmail.com>
2025-05-21 14:39:58 +0200

    Update authRoutes.js

--- dynamic-curriculum-saas/backend/routes/authRoutes.js
+++ dynamic-curriculum-saas/backend/routes/authRoutes.js
@@ -6,5 +6,6 @@ const { protect } = require('../middleware/authMiddleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);
+router.post('/employee/login', loginEmployee);
 
 module.exports = router;
